b48a27e0a50fe9681d60c153252e7e61ef08f0dc
stronkelicious <hugo.lindmark@gmail.com>
2019-11-27 13:28:18 +0100

    Final touch

--- src/se/nackademin/Client.java
+++ src/se/nackademin/Client.java
@@ -22,34 +22,23 @@ public class Client {
             Response fromServer;
 
             System.out.println("Show waiting view");
-            // call the categories
 
             while ((fromServer = (Response) inFromServer.readObject()) != null) {
-                System.out.println("looping");
                 if (fromServer.getCategories() != null) {
-                    System.out.println("kategorier sektion ");
                     gui.setCategories((ArrayList<String>) fromServer.getCategories());
-                    // switch panels to Category view
-                    gui.switchTo(gui.categoryPanel); //reset
-
+                    gui.switchTo(gui.categoryPanel);
                 } else if (fromServer.getQuestion() != null) {
-                    // get the list with answers
-                    // and the correct answer and the question
                     Question question = fromServer.getQuestion();
                     gui.setQuestion(question.getQuestion());
-                    System.out.println("Question sektion " + question.getQuestion());
                     ArrayList<String> answers = question.getAnswers();
                     Collections.shuffle(answers);
                     gui.setAnswerAlternatives(answers);
                     gui.setCorrectAnswer(question.getRightAnswer());
-                    // switch panels to Game view
                     gui.switchTo(gui.gamePanel);
 
                 } else if (fromServer.getWaitingView()) {
                     System.out.println("showing waiting view");
-                    // switch panels to Waiting view
                     gui.switchTo(gui.lobbyPanel);
-                    // he has to wait
                 } else if (fromServer.getMyScore() != null && fromServer.getOpponentScore() != null) {
                     if (fromServer.getMyScore() > fromServer.getOpponentScore()) {
                         gui.showResult("You won by " + fromServer.getMyScore() + "-" + fromServer.getOpponentScore() + "!");
@@ -62,17 +51,15 @@ public class Client {
                 }
 
                 if (!fromServer.getWaitingView()) {
-                    // get response and while loop hasresponded == true
                     while (!gui.hasResponded()) {
                         try {
                             Thread.sleep(100);
-                        }catch (InterruptedException e){
+                        } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
-                        }
+                    }
                     if (gui.hasResponded()) {
                         String toServer = gui.getResponse();
-                        System.out.println(toServer);
 
                         if (fromServer.getQuestion() != null) {
                             try {
@@ -84,7 +71,6 @@ public class Client {
                                 outToServer.println("rätt");
                             } else {
                                 outToServer.println("fel");
-                                System.out.println("Skicka QuizGUI response to server");
                             }
                         } else {
                             outToServer.println(toServer);
